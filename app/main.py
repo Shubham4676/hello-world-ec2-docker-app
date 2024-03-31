614bb405886565c07b7248404650c4b529342aca
Deepak Vyas <54551443+InCloudWithDeepak@users.noreply.github.com>
2024-03-31 21:54:54 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hii from Python app !!!!"
+    return "Hii from Python apppppp !!!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
